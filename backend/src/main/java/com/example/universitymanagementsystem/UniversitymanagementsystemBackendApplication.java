ddfad5cde7b1935248a1c538c03301549105dc5e
Seetha Ram Rao Vanam <seetharam2602@gmail.com>
2025-02-21 00:28:53 -0600

    Initial Commit

---
+++ backend/src/main/java/com/example/universitymanagementsystem/UniversitymanagementsystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.universitymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversitymanagementsystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversitymanagementsystemBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/universitymanagementsystem/UniversitymanagementsystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.universitymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversitymanagementsystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
